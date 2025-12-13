e04775660c5c965f787c958c72a13d85f052af1f
donny <16478950+donny2025@user.noreply.gitee.com>
2025-12-14 00:00:56 +0800

    commit code

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import DemoSetEffect from './views/DemoSetEffect'
 import DemoSetRef from './views/DemoSetRef'
 import DemosetRef2 from './views/DemoSetRef2'
 import DemoSetMemo from './views/DemoSetMemo'
+import DemoCustomHook from './views/DemoCustomHook'
 
 // import { CountdownTimer } from './views/DemoTimer'
 const root = createRoot(document.getElementById('root'))
 root.render(
   <>
-    <DemoSetMemo></DemoSetMemo>
+    <DemoCustomHook></DemoCustomHook>
   </>
 )
 
