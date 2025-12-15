44ccd597a7704a9174b6e8580b45d22e879f98d3
donny <donnyliu8@gmail.com>
2025-12-15 11:52:38 +0800

    add demo grid

---
+++ src/views/DemoGrid.jsx
@@ -0,0 +1,23 @@
+
+
+const DemoGrid = () => { 
+    return (
+        <div>
+            <table border="1">
+                <tr>
+                    <th>first</th>
+                    <th>second</th>
+                </tr>
+                <tr>
+                    <td>1-1</td>
+                    <td>1-2</td>
+                </tr>
+                <tr>
+                    <td>2-1</td>
+                    <td>2-2</td>
+                </tr>
+            </table>
+        </div>
+    )
+}
+export default DemoGrid;
