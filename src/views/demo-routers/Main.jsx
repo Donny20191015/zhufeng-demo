293972a7069d96fb061aa090ed034bf0c73ad2fa
donny <donnyliu8@gmail.com>
2025-12-19 10:41:45 +0800

    add

--- src/views/demo-routers/Main.jsx
+++ src/views/demo-routers/Main.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Parent from './Parent';
 import Child1 from './Child1';
 import Child2 from './Child2';
@@ -8,11 +8,13 @@ const Main = () => {
     return (
         <Router>
             <Routes>
+                <Route path="/home" element={ <Navigate to="/" replace />} />
                 <Route path="/" element={ <Parent />} />
                 <Route path="/child1" element={ <Child1 />} />
                 <Route path="/child2/:id" element={ <Child2 />} >
                     <Route path="child2-1" element={ <Child2_1 /> } />
                 </Route>
+                <Route path="*" element={ <Navigate to="/" replace />} />
             </Routes>
         </Router>
     );
